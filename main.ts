b1034cebf2ba3b2610ed3ee8c91a3bb5cb93993f
atcvetkov <63304837+atcvetkov@users.noreply.github.com>
2024-12-26 19:40:44 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+function blink () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+}
+basic.forever(function () {
+    blink()
+})
